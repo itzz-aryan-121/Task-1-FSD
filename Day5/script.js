0b4eccc26569bdbfac01f9863d2864f4c6729949
Aryan <aryantomar239@gmail.com>
2024-10-09 13:06:03 +0530

    tttt

---
+++ Day5/script.js
@@ -0,0 +1,62 @@
+// var a = 12;
+// var a = 45; 
+// var b = "Hello"
+
+// var c = [1,2,3]
+
+// let e = {
+//     name: "Aryan",
+//     id: 12
+// }
+
+// let f = true
+
+
+// let x = "12"/"6";
+// let bol = "Aryan";
+// let str = String(bol)
+// console.log(str.charAt(1))
+
+// function init() {
+    
+//     let x =  confirm("Are You sure")
+//     // console.log(a)
+
+//     if(!x){
+//         let x = prompt("Okiee bye")
+//         console.log(x)
+        
+//     }else{
+//         alert("good Day")
+//     }
+// }
+
+// init()
+
+// console.log(a)
+
+
+
+// let str = "12abh"
+// let num = parseInt(str)
+
+
+// console.log(num)
+
+
+// let a = "12"
+// let b = 12.0
+
+// console.log(a===b)
+
+
+function validate(){
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+    if(username===password){
+        alert("Login Successfull")
+    }else{
+        alert("Login Failed")
+    }
+}
+
