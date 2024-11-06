b593138ca5f7719df389139547262a619869b226
Aryan Tomar <aryantomar239@gmail.com>
2024-11-06 13:07:50 +0530

    .

---
+++ Day10/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.css'
+
+const App = () => {
+  return (
+    <div className='gradient-background'>
+    <h1>Hello Everyone</h1>
+    <h4>It's Aryan Tomar</h4>
+
+    </div>
+
+  )
+}
+
+export default App
